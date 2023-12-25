0bee163f696259167cefce6e2c62adb8f618468a
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-12-25 18:07:51 +0100

    Styling Signup Page

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,7 +2,9 @@ function getSignup(req, res) {
   res.render("customer/auth/signup");
 }
 
-function getLogin(req, res) {}
+function getLogin(req, res) {
+  res.render("customer/auth/login");
+}
 
 function getHome(req, res) {
   res.render("customer/home/home");
