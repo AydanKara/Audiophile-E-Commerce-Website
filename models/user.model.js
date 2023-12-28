a158210b7730f6791262514f762aed0d4407e639
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-12-28 22:08:55 +0100

    Error Handling
    User Input Validation and Footer Style

--- models/user.model.js
+++ models/user.model.js
@@ -16,6 +16,15 @@ class User {
   getUserWithSameEmail() {
     return db.getDb().collection("users").findOne({ email: this.email });
   }
+
+  async existsAlready() {
+    const existingUser = await this.getUserWithSameEmail();
+    if (existingUser) {
+      return true;
+    }
+    return false;
+  }
+
   async signup() {
     const hashedPassword = await bcrypt.hash(this.password, 12);
     await db.getDb().collection("users").insertOne({
@@ -27,7 +36,7 @@ class User {
   }
 
   hasMatchingPassword(hashedPassword) {
-    return bcrypt.compare(this.password, hashedPassword)
+    return bcrypt.compare(this.password, hashedPassword);
   }
 }
 
