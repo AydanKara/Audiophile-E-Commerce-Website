f41c79685568226ab324b41a35fffcaa97bf2d2b
AydanKara <113279073+AydanKara@users.noreply.github.com>
2023-12-29 11:33:01 +0100

    Admin Authorization & Protected Navigation

--- util/authentication.js
+++ util/authentication.js
@@ -1,13 +1,14 @@
 function createUserSession(req, user, action) {
   req.session.uid = user._id.toString();
+  req.session.isAdmin = user.isAdmin;
   req.session.save(action);
 }
 
 function destroyUserAuthSession(req) {
-    req.session.uid = null;
+  req.session.uid = null;
 }
 
 module.exports = {
   createUserSession,
-  destroyUserAuthSession
+  destroyUserAuthSession,
 };
